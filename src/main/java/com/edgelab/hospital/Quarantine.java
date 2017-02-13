2bb7d166d24c38737cfc075d58edc807bb378a9a
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2017-02-13 19:34:50 +0600

    updated the design suggestion

--- src/main/java/com/edgelab/hospital/Quarantine.java
+++ src/main/java/com/edgelab/hospital/Quarantine.java
@@ -8,6 +8,46 @@ import java.util.Map;
 
 
 
+
+/*
+So yes, your code lack of Object Oriented design and patterns. A quick look give 
+me the idea that the Quarantine can be a factory for Medicine, but that is a little 
+improvement.
+
+The first thing to do is to create your models. You have the Quarantine class but also 
+a group of Patients, some Medecine and a set of Disease.
+
+The Disease are a facts, something that you will just accept, so it can be an enumeration. 
+[pattern enumeration]
+
+The Patients maintains a counter, that can move from one group to the other. It just 
+maintains an integer an can be seen as a decorator. [pattern decorator]
+
+class Patients {
+   int size;
+   void becomes(Patients other) {
+     other.size += this.size;
+     this.size = 0;
+   }
+}
+
+The Quarantine distribute one or more Medicine to all the Patients, I have created a Treatment 
+class to maintains all the Medicine. This class is immutable and allows me to query it and add 
+or remove Medicine. The quarantine can be a factory and a builder (because it compose the treatment).
+
+The new Treatment class can be seen as many patterns, helper or state]. But also a composite because 
+it implements the Medicine but is also a delegate.
+
+Finally, the biggest part is from the Medicine where all the logic reside. When add to the Treatment 
+a Medicine will change the results of it. So a Medicine must be combined with an existing Treatment 
+and given to a group of Patients to produce his effects. The strategy pattern apply to this class.
+
+interface Medicine {
+  void on(Quarantine quarantine);
+  Treatment combine(Treatment treatment);
+}
+*/
+
 /*
 Here is some feedback:
 
